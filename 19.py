86837b19846cae559fdb902044917779aea54725
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-11 19:49:30 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,11 @@
+sandy11=int(input())
+for j in range(2,sandy11+1):
+  if(sandy11%j==0):
+      sandy2=0
+      for m in range(2,j+1):
+          if(j%m==0) and (m!=j):
+              inp2=1
+              break
+      if(sandy2==0):
+          print(j,end=' ')
+       

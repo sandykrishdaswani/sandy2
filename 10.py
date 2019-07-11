77fdf26ed0aa9a9ea23fb8040c8c7983961ed44b
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-11 16:34:42 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,14 @@
+ps=input()
+pss=ps.split()
+san=ps[0]
+s=ps[1]
+count=0
+i=0
+while(i<len(san) and count<2):
+    if(san[i]!=s[i]):
+        count+=1
+    i+=1
+if(count==1 or count==0):
+    print("yes")
+else:
+    print("no")
